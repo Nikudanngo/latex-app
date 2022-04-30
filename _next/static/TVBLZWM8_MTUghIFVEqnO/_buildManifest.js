e72290799c5e91a74d15c7c6e1fdec5c83c8da15
Nikudanngo <Nikudanngo@users.noreply.github.com>
2022-04-30 06:04:00 +0000

    deploy: d69246df5da16cfc29550b2edecd6cf84963a71d

---
+++ _next/static/TVBLZWM8_MTUghIFVEqnO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812cee03-e728548d29ecfd66.js","static/css/1bdfa352808526ad.css","static/chunks/pages/index-35a13d460007bb10.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
